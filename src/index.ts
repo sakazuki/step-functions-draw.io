5b4a5cdf4f0d2cc1f70529d7af4ea5981eaf67ec
sakazuki <sakazuki@gmail.com>
2019-09-23 07:51:54 +0900

    change cdn provider from rawgit to jsdelivr

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-mxscript("https://cdn.rawgit.com/soney/jsep/b8baab7b/build/jsep.min.js");
-mxscript("https://cdn.rawgit.com/nodeca/js-yaml/9c1894e2/dist/js-yaml.min.js");
-mxscript("https://sdk.amazonaws.com/js/aws-sdk-2.510.0.min.js");
-// mxscript("https://rawgit.com/soney/jsep/master/build/jsep.min.js");
-// mxscript("https://f248fda6.ngrok.io/aws-sdk-2.510.0.js");
-// mxscript("https://localhost:8000/js/jsep.min.js");
-mxscript("https://cdn.jsdelivr.net/npm/jsoneditor@7.0.4/dist/jsoneditor.min.js");
-
-var linkElement = document.createElement('link');
-linkElement.setAttribute('rel', 'stylesheet');
-linkElement.setAttribute('href', 'https://cdn.jsdelivr.net/npm/jsoneditor@7.0.4/dist/jsoneditor.min.css');
-document.getElementsByTagName('head')[0].appendChild(linkElement);
-
+import { linkStyle } from "./utils";
 import main from "./main";
+
+mxscript("https://cdn.jsdelivr.net/npm/jsep@0.3.4/build/jsep.min.js");
+mxscript("https://cdn.jsdelivr.net/npm/js-yaml@3.13.1/dist/js-yaml.min.js");
+mxscript("https://sdk.amazonaws.com/js/aws-sdk-2.510.0.min.js");
+mxscript("https://cdn.jsdelivr.net/npm/jsoneditor@7.0.4/dist/jsoneditor.min.js");
+linkStyle('https://cdn.jsdelivr.net/npm/jsoneditor@7.0.4/dist/jsoneditor.min.css');
 
 Draw.loadPlugin(main);
 
